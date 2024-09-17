d46779c3694f6d4f7d034c2aaa120f77f827dee6
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2024-09-17 13:34:33 -0400

    Added camera movement
    Used Cinemachine

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float moveSpeed = 5f;
+
     Vector2 moveDirection = Vector2.zero;
+    Transform cameraTransform;
+    Vector3 playerVelocity; 
 
     Inputs playerInputs;
     InputAction move;
@@ -17,27 +20,29 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        cameraTransform = Camera.main.transform;
         playerInputs = new Inputs();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void OnEnable()
     {
         move = playerInputs.Player.Move;
-        move.Enable();
+        move.Enable();        
+
+        look = playerInputs.Player.Look;
+        look.Enable();
 
         shoot = playerInputs.Player.Fire;
         shoot.Enable();
         shoot.performed += Shoot;
-
-        look = playerInputs.Player.Look;
-        look.Enable();
     }
 
     private void OnDisable()
     {
         move.Disable();
-        shoot.Disable();
         look.Disable();
+        shoot.Disable();
     }
 
     void Update()
@@ -47,7 +52,10 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector3(moveDirection.x * moveSpeed, rb.velocity.y, moveDirection.y * moveSpeed);
+        Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y);
+        movement = cameraTransform.forward * movement.z + cameraTransform.right * movement.x;
+        movement.y = 0f;
+        rb.velocity = movement * moveSpeed;
     }
 
     private void Shoot(InputAction.CallbackContext context)
