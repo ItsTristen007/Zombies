8f3d4b61d66b933ae60506bebe0fff55ee2bae19
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-01-11 18:31:30 -0500

    Added camera shake when damaged

--- Assets/Scripts/Menu/UIManager.cs
+++ Assets/Scripts/Menu/UIManager.cs
@@ -1,8 +1,8 @@
-using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class UIManager : MonoBehaviour
 {
