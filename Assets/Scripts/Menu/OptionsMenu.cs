6d58376c92610a37562c56a4dee2c8e8f59f6644
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-01-12 17:19:49 -0500

    Changed enemy materials, and added options menu
    Options menu is not functional yet. Also made the main level smaller.

---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider xSensitivitySlider;
+    [SerializeField] Slider ySensitivitySlider;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
