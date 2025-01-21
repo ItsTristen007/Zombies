6f40e931f91816afd62f989184f428e1b5e7d1d5
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-01-20 19:08:32 -0500

    Added new sounds for the pistol and shotgun

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -10,6 +10,9 @@ public class PlayerWeapon : MonoBehaviour
     [SerializeField] float bulletDamage = 5f;
     [SerializeField] float fireRate = 0.1f;
     [SerializeField] float reloadSpeed = 1f;
+    bool usingPistol;
+    bool usingShotgun;
+    bool usingSMG;
 
     public float GetMaxAmmo()
     {
@@ -51,9 +54,27 @@ public class PlayerWeapon : MonoBehaviour
         return reloadSpeed;
     }
 
+    public bool GetUsingPistol()
+    {
+        return usingPistol;
+    }
+
+    public bool GetUsingShotgun()
+    {
+        return usingShotgun;
+    }
+
+    public bool GetUsingSMG()
+    {
+        return usingSMG;
+    }
+
     void Awake()
     {
         currentAmmo = maxAmmo;
+        usingPistol = true;
+        usingShotgun = false;
+        usingSMG = false;
     }
 
     public void SwitchToPistol()
@@ -64,6 +85,9 @@ public class PlayerWeapon : MonoBehaviour
         reloadSpeed = 1.5f;
 
         currentAmmo = maxAmmo;
+        usingPistol = true;
+        usingShotgun = false;
+        usingSMG = false;
     }
 
     public void SwitchToShotgun()
@@ -74,6 +98,9 @@ public class PlayerWeapon : MonoBehaviour
         reloadSpeed = 2.5f;
 
         currentAmmo = maxAmmo;
+        usingPistol = false;
+        usingShotgun = true;
+        usingSMG = false;
     }
 
     public void SwitchToSMG()
@@ -84,5 +111,8 @@ public class PlayerWeapon : MonoBehaviour
         reloadSpeed = 1f;
 
         currentAmmo = maxAmmo;
+        usingPistol = false;
+        usingShotgun = false;
+        usingSMG = true;
     }
 }
