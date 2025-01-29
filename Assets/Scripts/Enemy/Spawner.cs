44371faed224de7e1bce9ae79a4a0e42bf150e20
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-01-29 18:31:50 -0500

    Changed enemy spawning system
    Now, everything is done in the SpawnManager script

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -5,77 +5,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-    [SerializeField] GameObject newWaveText;
-    [SerializeField] float timeBeforeSpawn = 1.5f;
-    [SerializeField] float timeBetweenWaves = 8f;
-    [SerializeField] int waveSpawnNumber;
-    GameObject[] enemies;
-    int numSpawned;
-    bool isSpawning = true;
-    bool isWaiting;
-    bool waveStarting;
 
-    public bool GetWaveStarting()
+    public void SpawnEnemy()
     {
-        return waveStarting;
-    }
-
-    public void SetWaveStarting(bool newState)
-    {
-        waveStarting = newState;
-    }
-
-    void Awake()
-    {
-        newWaveText.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (isSpawning && numSpawned < waveSpawnNumber)
-        {
-            Instantiate(enemy, transform.position + Vector3.up, Quaternion.identity);
-            numSpawned++;
-            isSpawning = false;
-        }
-        else if (!isSpawning)
-        {
-            if (!isWaiting)
-            {
-                StartCoroutine(SpawnWaitTime());
-            }
-        }
-        else
-        {
-            enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            if (enemies.Length == 0 && !isWaiting)
-            {
-                StartCoroutine(TimeUntilNewWave());
-            }
-        }
-    }
-
-    IEnumerator SpawnWaitTime()
-    {
-        isWaiting = true;
-        yield return new WaitForSeconds(timeBeforeSpawn);
-        isSpawning = true;
-        isWaiting = false;
-    }
-
-    IEnumerator TimeUntilNewWave()
-    {
-        isWaiting = true;
-        newWaveText.SetActive(true);
-        yield return new WaitForSeconds(timeBetweenWaves);
-        numSpawned = 0;
-        if (enemy.name == "CommonEnemy")
-        {
-            waveSpawnNumber++;
-        }
-        isSpawning = true;
-        isWaiting = false;
-        waveStarting = true;
-        newWaveText.SetActive(false);
+        Instantiate(enemy, transform.position + Vector3.up, Quaternion.identity);
     }
 }
